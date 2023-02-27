e11f54b4e63a596747ed36e26ffc30befbb28eb1
Harold Duarte <105592122+Harold29j@users.noreply.github.com>
2023-02-26 23:11:57 -0500

    Add files via upload

---
+++ parabola.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.animation as animation
+#Definimos los paramentros
+
+root = tk.Tk()
+root.config(width=725, height=525)
+theta=np.pi/3
+x0=0.
+y0=0.
+v0=30.
+g=9.8
+
+def x_pos(theta,t,v0,x0):
+    x=x0+v0*np.cos(theta)*t
+    return x
+
+def y_pos(theta,t,v0,y0):
+    y=y0+(v0*np.sin(theta)*t)-((g*t**2)/2)
+    return y
+
+time = ((2*v0)*np.sin(theta))/g
+t=np.linspace(0,time,50)
+x=x_pos(theta,t,v0,0)
+y=y_pos(theta,t,v0,0)
+N=len(t)
+
+fig, ax=plt.subplots()
+ln, = plt.plot(x,y,'ro')
+ax.set_xlim(0,100)
+ax.set_ylim(0,100)
+
+def actualizar(i):
+    ln.set_data(x[i],y[i])
+    return ln,
+
+ani = animation.FuncAnimation(fig,actualizar,range(N),interval=1, repeat = False)
+
+plt.grid()
+plt.show()
